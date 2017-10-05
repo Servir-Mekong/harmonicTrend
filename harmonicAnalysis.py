449c190161d11767ea2452f2aeb935a121c2be41
apoortinga <poortinga.ate@gmail.com>
2017-10-05 15:59:48 +0700

    included cos sin functions

--- harmonicAnalysis.py
+++ harmonicAnalysis.py
@@ -9,6 +9,10 @@ import ee
 import logging
 import time
 import math
+from pylab import *
+from scipy.optimize import newton_krylov
+import numpy as np
+import matplotlib.pyplot as plt
 
 class environment(object):
     
@@ -93,10 +97,80 @@ class harmonicTrend():
 	harmonicTrend = harmonicLandsat.select(self.independents.add(self.env.dependent)).reduce(ee.Reducer.linearRegression(self.independents.length(), 1))
 
 	## Turn the array image into a multi-band image of coefficients.
-	harmonicTrendCoefficients = harmonicTrend.select('coefficients').arrayProject([0]).arrayFlatten([self.independents])
+	harmonicTrendCoefficients = harmonicTrend.select('coefficients').arrayProject([0]).arrayFlatten([self.independents]).clip(self.env.location)
+	#print harmonicTrendCoefficients
+	raw_list = harmonicTrendCoefficients.reduceRegion(reducer=ee.Reducer.toList(),geometry=self.env.location,maxPixels=5e9,scale=500);
 	
-	outputName = "Harmonic_trend_" + str(self.env.startYear)
-	self.ExportToAsset(harmonicTrendCoefficients,outputName)         
+	start = 33 #*2*np.pi
+	stop =  34 #*2*np.pi
+	t = np.arange(start, stop, 0.005)  
+	i = 300
+	cos1 = ee.Array(raw_list.get('cos_0')).getInfo()[i]
+	cos2 = ee.Array(raw_list.get('cos_1')).getInfo()[i]
+	cos3 = ee.Array(raw_list.get('cos_2')).getInfo()[i]
+
+	sin1 = ee.Array(raw_list.get('sin_0')).getInfo()[i]
+	sin2 = ee.Array(raw_list.get('sin_1')).getInfo()[i]
+	sin3 = ee.Array(raw_list.get('sin_2')).getInfo()[i]
+	
+	constant = ee.Array(raw_list.get('constant')).getInfo()[i]
+	print constant
+
+	#print len(cos1),len(cos2), len(cos3), len(sin1),len(sin2), len(sin3), len(constant)
+	
+	w = 1                # number of cycles
+	xt1 = cos1*np.cos(2*np.pi*w*t) + sin1*np.sin(2*np.pi*w*t)
+	w = 2                # number of cycles
+	xt2 = cos2*np.cos(2*np.pi*w*t) + sin2*np.sin(2*np.pi*w*t) 
+	w = 3                # number of cycles
+	xt3 = cos3*np.cos(2*np.pi*w*t) + sin3*np.sin(2*np.pi*w*t)
+	
+	
+	xt = constant*t+xt1+xt2+xt3
+	
+	DT1 = -2*np.pi*cos1*np.sin(2*np.pi*t) + 2*np.pi*sin1*np.cos(2*np.pi*t)
+	DT2 = -4*np.pi*cos2*np.sin(4*np.pi*t) + 4*np.pi*sin2*np.cos(4*np.pi*t)
+	DT3 = -6*np.pi*cos3*np.sin(6*np.pi*t) + 6*np.pi*sin3*np.cos(6*np.pi*t)
+
+
+	dtdx = -constant + DT1 + DT2 + DT3
+	
+	fig, ax1 = plt.subplots()
+	ax1.plot(t, xt, 'b-')
+
+	ax2 = ax1.twinx()
+	s2 = np.sin(2 * np.pi * t)
+	ax2.plot(t, dtdx, 'r-')
+
+	fig.tight_layout()
+	plt.axhline(y=0, color='r', linestyle='-')
+
+	def F(t):
+	    #y = np.sin(np.pi*t)
+	    #return np.power(y,2) - 1 + np.power(((b1+2*np.pi*y*(12*b6*np.power(y,2)-4*b5-y-b2+2*b5-9*b6)) / (2*np.pi*(b3-4*b4*y +3*b7*(1-4*np.power(y,2))))),2)
+	    return constant-np.pi*cos1*np.sin(2*np.pi*t) + 2*np.pi*sin1*np.cos(2*np.pi*t)  - 4*np.pi*cos2*np.sin(4*np.pi*t) + 4*np.pi*sin2*np.cos(4*np.pi*t) -6*np.pi*cos3*np.sin(6*np.pi*t) + 6*np.pi*sin3*np.cos(6*np.pi*t)
+
+	#"""
+	intersections = []
+	myRange = np.arange(start, stop, 0.1)    
+	for i in myRange:
+	    try:
+		sol = newton_krylov(F, i,  verbose=1)
+		if sol > start and sol < stop:
+		    #print sol
+		    plt.axvline(x=sol, color='k', linestyle='--')
+		    intersections.append(float(sol))  
+		    #print sol	
+	    except:
+		pass
+	#"""		
+	plt.show()
+	
+	
+	#df = DataFrame(band1, columns = pts[0])
+	#print df
+	#outputName = "Harmonic_trend_" + str(self.env.startYear)
+	#self.ExportToAsset(harmonicTrendCoefficients,outputName)         
 	
 	##fittedHarmonic = harmonicLandsat.map(self.fitLandsat)
 	## Compute the number of observations in each pixel.
