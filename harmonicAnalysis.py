e6b2cdf54ed67f2fd6a16106c1e4c898b9eecf26
apoortinga <poortinga.ate@gmail.com>
2017-10-16 16:00:38 +0700

    added two seasons

--- harmonicAnalysis.py
+++ harmonicAnalysis.py
@@ -27,8 +27,8 @@ class environment(object):
         self.timeString = time.strftime("%Y%m%d_%H%M%S")
 
         # set dates
-        self.startYear = 2003;
-        self.endYear = 2003;
+        self.startYear = 2005;
+        self.endYear = 2005;
 
         # construct date objects
         startDate = ee.Date.fromYMD(self.startYear,1,1)
@@ -39,14 +39,19 @@ class environment(object):
 	MYD = ee.ImageCollection("MODIS/006/MYD13Q1")
 
         # set location 
-        self.location = ee.Geometry.Polygon([[[105.47321319580078,19.230770079948247],[105.46428680419922,18.781841765942392],[105.58891296386719,18.78411703953223],[105.6005859375,19.21391262405755],[105.47321319580078,19.230770079948247]]])
+        #self.location = ee.Geometry.Polygon([[[105.207,17.854],[105.597,17.869],[105.617,19.243],[105.257,19.241],[105.207,17.854]]])
 
-	#self.location = ee.Geometry.Polygon([[[103.294,17.923],[103.294,17.923],[106.453,17.923],[106.453,20.469],[103.2941,20.469],[103.294,17.923]]])
-	#self.location = ee.Geometry.Polygon([[[103.952,18.646],[106.875,18.604],[107.072,21.943],[103.623,22.187],[103.952,18.646]]])
-	#self.location = ee.Geometry.Polygon([[[105.633,18.761219005674416],[105.648,18.766826560475042],[105.645,18.78056021852182],[105.633,18.782327634650493],[105.633,18.761219005674416]]])
-	self.location =  ee.Geometry.Polygon([[[105.12542724609375,18.651449894396634],[105.70770263671875,18.61501328321048],[105.6390380859375,19.21780295966795],[105.22430419921875,19.210022196386085],[105.12542724609375,18.651449894396634]]])
+	# Nghe an
+	self.location =ee.Geometry.Polygon([[[103.294,17.923],[103.294,18.923],[106.453,17.923],[106.453,23.469],[103.2941,23.469],[103.294,17.923]]])
+
+	
+	#self.location = ee.Geometry.Polygon([[[104.8040771484375,10.428390862637604],[105.611572265625,10.201406794334043],[106.5069580078125,10.493213228263972], [106.710205078125,11.08138460241306],[104.7381591796875,11.016688524459864],[104.8040771484375,10.428390862637604]]])
+	#self.location =  ee.Geometry.Polygon([[[105.12542724609375,18.651449894396634],[105.70770263671875,18.61501328321048],[105.6390380859375,19.21780295966795],[105.22430419921875,19.210022196386085],[105.12542724609375,18.651449894396634]]])
 	# Vietnam
-	self.location =  ee.Geometry.Polygon([[[102.1405029,8.574163399999973],[109.4590988,8.574163399999973], [109.4590988,23.375820100000023], [102.1405029,23.375820100000023], [102.1405029,8.574163399999973]]])
+	#self.location =  ee.Geometry.Polygon([[[102.1405029,8.574163399999973],[109.4590988,8.574163399999973], [109.4590988,23.375820100000023], [102.1405029,23.375820100000023], [102.1405029,8.574163399999973]]])
+	
+	# cambodia
+	#self.location =  ee.Geometry.Polygon([[[103.3514022,10.009719799999973],[107.63639830000001,10.009719799999973],[107.63639830000001,14.704959800000026],[103.3514022,14.704959800000026],[103.3514022,10.009719799999973]]])
 
 
 	mod13 = ee.ImageCollection(MOD.merge(MYD)).filterDate(startDate,endDate);
@@ -89,20 +94,46 @@ class harmonicTrend():
     def runModel(self):
 		
 	print 'getting data'
-	EVI = self.env.mod13.map(self.scaling);
+	self.EVI = self.env.mod13.map(self.scaling);
 	
-	self.harmonicFrequencies = ee.List.sequence(1, self.env.harmonics);
+	cycles = 2
+	intersectionPoints, eviValues, lats, lons = self.applyHarmonics(cycles)
+	
+	self.exportToDrive(intersectionPoints,"days",cycles,lats,lons)	
+	self.exportToDrive(eviValues,"EVI",cycles,lats,lons)	
+	
+	cycles = 3
+	intersectionPoints, eviValues, lats, lons  = self.applyHarmonics(cycles)
+	
+	self.exportToDrive(intersectionPoints,"days",cycles,lats,lons)		
+	self.exportToDrive(eviValues,"EVI",cycles,lats,lons)	
+	
+	#constants =  np.array(constants) # / (2*np.pi)
+
+	return intersectionPoints
+	# in case you want to plot the image
+	#import matplotlib.pyplot as plt        
+	#plt.imshow(arr,  vmin=0, vmax=365)
+	#plt.show()
+	#return arr 
+	
+
+    def applyHarmonics(self,cycles):
+	
+	self.harmonicFrequencies = ee.List.sequence(1, cycles);
+	
+	self.env.harmonics = cycles
+      
       
         # Construct lists of names for the harmonic terms.
-	self.cosNames = ee.List(self.getNames('cos_', self.env.harmonics));
-	self.sinNames = ee.List(self.getNames('sin_', self.env.harmonics));
+	self.cosNames = ee.List(self.getNames('cos_', cycles));
+	self.sinNames = ee.List(self.getNames('sin_', cycles));
 	
 	## Independent variables.
 	self.independents = ee.List(['constant', 't']).cat(self.cosNames).cat(self.sinNames);
 	
-
 	## Filter to the area of interest, mask clouds, add variables.
-	harmonicLandsat = EVI.map(self.addConstant).map(self.addTime).map(self.addHarmonics);
+	harmonicLandsat = self.EVI.map(self.addConstant).map(self.addTime).map(self.addHarmonics);
 	
 	## The output of the regression reduction is a 4x1 array image.
 	harmonicTrend = harmonicLandsat.select(self.independents.add(self.env.dependent)).reduce(ee.Reducer.linearRegression(self.independents.length(), 1))
@@ -110,25 +141,37 @@ class harmonicTrend():
 	## Turn the array image into a multi-band image of coefficients.
 	harmonicTrendCoefficients = harmonicTrend.select('coefficients').arrayProject([0]).arrayFlatten([self.independents])
 	
+	# map with number of seasons = 
+	seasonsinyear = "users/servirmekong/NumberOfSeasons/seasons_" + str(self.env.startYear )
+	rsq = ee.Image(seasonsinyear).select('cycles')
+	
 	# get the lat lon and add the ndvi
-	latlon = ee.Image.pixelLonLat().addBands(harmonicTrendCoefficients).unmask(-9999)
+	latlon = ee.Image.pixelLonLat().addBands(harmonicTrendCoefficients).addBands(rsq).unmask(-9999)
 				
 	#print harmonicTrendCoefficients
 	latlon = latlon.reduceRegion(reducer=ee.Reducer.toList(),geometry=self.env.location,maxPixels=5e9,scale=self.env.scale);
+		
+	# get the number of cycles
+	rsqdata =  np.array((ee.Array(latlon.get('cycles')).getInfo()))
 	
 	print 'data to numpy'		
 	# get data into three different arrays
-	cos_0 = np.array((ee.Array(latlon.get("cos_0")).getInfo()))
-	cos_1 = np.array((ee.Array(latlon.get("cos_1")).getInfo()))
-	cos_2 = np.array((ee.Array(latlon.get("cos_2")).getInfo()))
-	sin_0 = np.array((ee.Array(latlon.get("sin_0")).getInfo()))
-	sin_1 = np.array((ee.Array(latlon.get("sin_1")).getInfo()))
-	sin_2 = np.array((ee.Array(latlon.get("sin_2")).getInfo()))
-	constants =  np.array((ee.Array(latlon.get('constant')).getInfo()))	
+	cos_0 = np.array((ee.Array(latlon.get("cos_0")).getInfo()))[rsqdata == cycles]
+	cos_1 = np.array((ee.Array(latlon.get("cos_1")).getInfo()))[rsqdata == cycles]
 	
+	sin_0 = np.array((ee.Array(latlon.get("sin_0")).getInfo()))[rsqdata == cycles]
+	sin_1 = np.array((ee.Array(latlon.get("sin_1")).getInfo()))[rsqdata == cycles]
+	
+	constants =  np.array((ee.Array(latlon.get('constant')).getInfo()))[rsqdata == cycles]	
+	
+	if cycles == 3:
+	    cos_2 = np.array((ee.Array(latlon.get("cos_2")).getInfo()))[rsqdata == cycles]
+	    sin_2 = np.array((ee.Array(latlon.get("sin_2")).getInfo()))[rsqdata == cycles]
+		
+	# get lat lon
 	self.lats = np.array((ee.Array(latlon.get("latitude")).getInfo()))
 	self.lons = np.array((ee.Array(latlon.get("longitude")).getInfo()))
-	 
+	 	 
 	# get the unique coordinates
 	self.uniqueLats = np.unique(self.lats)
 	self.uniqueLons = np.unique(self.lons)
@@ -147,21 +190,22 @@ class harmonicTrend():
 	# start and end date are calculate from 1970 * 2* pi
 	self.start = 0 #int(self.env.startYear - 1970)
 	self.stop =  1 #int(self.env.endYear - 1970)+1
-	
-	#constants =  np.array(constants) # / (2*np.pi)
-	print 'calc intersections'	
-	intersectionPoints, eviValues = self.calculateDates(cos_0,cos_1,cos_2,sin_0,sin_1,sin_2,constants)
-	
-	self.exportToDrive(intersectionPoints,"days")	
-	self.exportToDrive(eviValues,"EVI")	
 
-	return intersectionPoints
-	# in case you want to plot the image
-	#import matplotlib.pyplot as plt        
-	#plt.imshow(arr,  vmin=0, vmax=365)
-	#plt.show()
-	#return arr 
+	# do two cycle analysis
 	
+	lats = self.lats[rsqdata == cycles]
+	lons = self.lons[rsqdata == cycles]
+	
+	if cycles == 2:
+	    print 'calc inflectionpoints', cycles
+	    intersectionPoints, eviValues = self.calculateDates2Cycles(cos_0,cos_1,sin_0,sin_1,constants)	
+	    
+	if cycles == 3:
+	    print 'calc inflectionpoints', cycles
+	    intersectionPoints, eviValues = self.calculateDates3Cycles(cos_0,cos_1,cos_2,sin_0,sin_1,sin_2,constants)
+	    
+	return intersectionPoints, eviValues, lats, lons
+
 
 
     def scaling(self,img):
@@ -214,7 +258,7 @@ class harmonicTrend():
 	return img.addBands(image)
 
 
-    def calculateDates(self,cos_0,cos_1,cos_2,sin_0,sin_1,sin_2,constants):
+    def calculateDates2Cycles(self,cos_0,cos_1,sin_0,sin_1,constants):
 	""" Calculate the min and max using non-linear solver. """
 	
 	# empty list to store results
@@ -223,54 +267,118 @@ class harmonicTrend():
 	
 	# range of values for non-linear solver
 	myRange = np.arange(self.start, self.stop, 0.1)  
-	
+
 	counter = 0
+
 	for x in range(0,len(cos_0)-1,1):
 	    counter +=1
 	    if counter > 1000:
-		print (x/len(cos_0))*100 , " .. % ", 
+		print (float(x)/float(len(cos_0)))*100.0 , " .. %\n ", 
 		counter = 0
 	    
-	    # empty list with interection points
+	    #empty list with interection points
 	    intersections = []
 	    evis = []
 	    
 	    for i in myRange:
-		try:
-		    # get all values
-		    cos1 = cos_0[x]
-		    cos2 = cos_1[x]
-		    cos3 = cos_2[x]
 
-		    sin1 = sin_0[x]
-		    sin2 = sin_1[x]
-		    sin3 = sin_2[x]
-		    constant = constants[x]
+		cos1 = cos_0[x]
+		cos2 = cos_1[x]
+
+		sin1 = sin_0[x]
+		sin2 = sin_1[x]
+		constant = constants[x]
 		    
-		    #print cos1, cos2, cos3, sin1, sin2, sin3, constant
 		    
-		    # function to be solved		    
-		    def derivative(t):
-			return constant-2*np.pi*cos1*np.sin(2*np.pi*t) + 2*np.pi*sin1*np.cos(2*np.pi*t) - 4*np.pi*cos2*np.sin(4*np.pi*t) + 4*np.pi*sin2*np.cos(4*np.pi*t) -6*np.pi*cos3*np.sin(6*np.pi*t) + 6*np.pi*sin3*np.cos(6*np.pi*t)
+		# function to be solved		    
+		def derivative(t):
+		 return constant-2*np.pi*cos1*np.sin(2*np.pi*t) + 2*np.pi*sin1*np.cos(2*np.pi*t) - 4*np.pi*cos2*np.sin(4*np.pi*t) + 4*np.pi*sin2*np.cos(4*np.pi*t)
 		
-		    def func(t):
-			return constant + cos1*np.cos(2*np.pi*1*t) + sin1*np.sin(2*np.pi*1*t) + cos2*np.cos(2*np.pi*2*t) + sin2*np.sin(2*np.pi*2*t) + cos3*np.cos(2*np.pi*3*t) + sin3*np.sin(2*np.pi*3*t)
+		def func(t):
+		    return constant + cos1*np.cos(2*np.pi*1*t) + sin1*np.sin(2*np.pi*1*t) + cos2*np.cos(2*np.pi*2*t) + sin2*np.sin(2*np.pi*2*t)  
 			
+		try:
 		    # the non-linear solver
-		    sol = newton_krylov(derivative, i,  verbose=0)
-		    
+		    sol = float(newton_krylov(derivative,i,  verbose=0,maxiter=100))
+			    
 		    # if the results is within the expected boundaries
 		    if sol > self.start and sol < self.stop:
-			intersections.append(float(sol))  
-			evis.append(func(float(sol)))
+			intersections.append(sol)  
+			evis.append(func(sol))
 			
 		
 		except:
 		    pass
+		    
 	    dayValues.append(intersections)
 	    eviValues.append(evis)
 	
 	return dayValues, eviValues
+
+    def calculateDates3Cycles(self,cos_0,cos_1,cos_2,sin_0,sin_1,sin_2,constants):
+	""" Calculate the min and max using non-linear solver. """
+	
+	# empty list to store results
+	dayValues = []
+	eviValues = []
+	
+	# range of values for non-linear solver
+	myRange = np.arange(self.start, self.stop, 0.1)  
+
+	counter = 0
+
+	for x in range(0,len(cos_0)-1,1):
+	    counter +=1
+	    if counter > 1000:
+		print (float(x)/float(len(cos_0)))*100.0 , " .. %\n ", 
+		counter = 0
+	    
+	    #empty list with interection points
+	    intersections = []
+	    evis = []
+	    
+	    for i in myRange:
+#		try:
+	    # get all values
+		cos1 = cos_0[x]
+		cos2 = cos_1[x]
+		cos3 = cos_2[x]
+
+		sin1 = sin_0[x]
+		sin2 = sin_1[x]
+		sin3 = sin_2[x]
+		constant = constants[x]
+		    
+	    #print cos1, cos2, sin1, sin2, constant
+		    
+	    # function to be solved		    
+		def derivative(t):
+		 return constant-2*np.pi*cos1*np.sin(2*np.pi*t) + 2*np.pi*sin1*np.cos(2*np.pi*t) - 4*np.pi*cos2*np.sin(4*np.pi*t) + 4*np.pi*sin2*np.cos(4*np.pi*t) -6*np.pi*cos3*np.sin(6*np.pi*t) + 6*np.pi*sin3*np.cos(6*np.pi*t)
+		
+		def func(t):
+		    return constant + cos1*np.cos(2*np.pi*1*t) + sin1*np.sin(2*np.pi*1*t) + cos2*np.cos(2*np.pi*2*t) + sin2*np.sin(2*np.pi*2*t)  + cos3*np.cos(2*np.pi*3*t) + sin3*np.sin(2*np.pi*3*t)
+			
+		try:
+		    # the non-linear solver
+		    sol = float(newton_krylov(derivative,i,  verbose=0,maxiter=100))
+	    
+		    #print sol
+			    
+		    # if the results is within the expected boundaries
+		    if sol > self.start and sol < self.stop:
+			intersections.append(sol)  
+			evis.append(func(sol))
+			
+		
+		except:
+		    pass
+		    
+	    #print intersections
+	    dayValues.append(intersections)
+	    eviValues.append(evis)
+	
+	return dayValues, eviValues
+
 
     def exportToGeoTiff(self,arr,number,name):
 	""" export the result to a geotif. """
@@ -289,7 +397,7 @@ class harmonicTrend():
 	driver = gdal.GetDriverByName('GTiff')
 
 	timestring = time.strftime("%Y%m%d_%H%M%S")
-	outputDataset = driver.Create(r"d:\mydata/" + str(name) + timestring + "band_"+ str(number) + ".tif", self.ncols,self.nrows, 1,gdal.GDT_Float32)
+	outputDataset = driver.Create(r"d:\mydata/maps/" + str(name) + timestring + "band_"+ str(number) + ".tif", self.ncols,self.nrows, 1,gdal.GDT_Float32)
 	print "exporting .. " + str("d:\mydata/" + timestring + "test.tif")
 
 	# add some metadata
@@ -301,35 +409,32 @@ class harmonicTrend():
 	outputDataset.GetRasterBand(1).SetNoDataValue(-9999)
 	outputDataset = None
 
-    def exportToDrive(self,values,name):
+    def exportToDrive(self,values,name,nr,lats,lons):
 	""" export the result to drive using geotiff export function. """
 	
 	
 	# we expect six intersections
-	for j in range(0,6,1):
+	for j in range(0,nr*2,1):
 	    # make an empty array with nrows and ncols
 	    
 	    arr = np.zeros([self.nrows,self.ncols])-9999
 	    # set counter at 0
 	    counter =0
 	    
-	    # loop over the array
+	    	    # loop over the array
 	    for y in range(0,len(arr),1):
 		for x in range(0,len(arr[0]),1):
-		    #try:
-			# if the lat and lon match the location in the grid
-			if self.lats[counter] == self.uniqueLats[y] and self.lons[counter] == self.uniqueLons[x] and counter < len(self.lats)-1:
+		    # if the lat and lon match the location in the grid
+		    if lats[counter] == self.uniqueLats[y] and lons[counter] == self.uniqueLons[x] and counter < len(lats)-1:
 			    
-			    # get unique intersection pints
-			    val = unique(np.round(values[counter],3))
-			    counter+=1
-			    # write to grid if we have six values.
-			    if len(val) == 6:
-				if val[j] > -1 and  val[j] < 1:
-				    arr[len(self.uniqueLats)-1-y,x] = (val[j]-self.start) # we start from lower left corner
-		  #  except:	
-		#	pass
-	    
+			# get unique intersection pints
+			val = unique(np.round(values[counter],3))
+			counter+=1
+			# write to grid if we have six values.
+			if len(val) == nr*2:
+			    #if val[j] > -1 and  val[j] < 1:
+				#print val[j]
+			    arr[len(self.uniqueLats)-1-y,x] = (val[j]-self.start) # we start from lower left corner
 	    # write the band to disk
 	    self.exportToGeoTiff(arr,j+1,name)
 	return arr
@@ -338,7 +443,7 @@ class harmonicTrend():
     def ExportToAsset(self,img,assetName):  
         """export to asset """
         
-        outputName = self.env.userID + str(self.env.timeString) + assetName
+        outputName = self.env.userID + str(nr) + "_" + str(self.env.timeString) + assetName
         logging.info('export image to asset: ' + str(outputName))   
 	
 	#img = img.multiply(10000).int16()
